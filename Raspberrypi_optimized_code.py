6729f159c2c87c40b4ea9db392c70e18f4b2e4f2
Fruitpunch44 <93434339+Fruitpunch44@users.noreply.github.com>
2024-06-04 10:44:53 +0100

    Update Raspberrypi_optimized_code.py
    removed the use of a gpio pin function cause it's really not needed

--- Raspberrypi_optimized_code.py
+++ Raspberrypi_optimized_code.py
@@ -1,40 +1,37 @@
-from flask import Flask, Response, redirect, url_for, jsonify, request, flash, render_template, session
-from flask_socketio import SocketIO
+# Main libraries that are used
 from functools import wraps
 import datetime
+import psutil
 import platform
-import os
-import cv2
-import RPI.GPIO as GPIO
-import numpy as np
 import yaml
-import requests
+import cv2
+import pygame
 from ultralytics import YOLO
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-from playsound import playsound
+import os
+from flask import (Flask, Response, redirect, url_for, jsonify, request, flash, render_template, session)
+from flask_socketio import SocketIO, emit
+import requests
+import numpy as np
 import firebase_admin
-from firebase_admin import credentials, db
+from firebase_admin import credentials
+from firebase_admin import db
+from picamera2 import Picamera2, MappedArray
 
-# Firebase configuration
+# important to use the REST API BY GOOGLE
 Firebase_api_key = 'AIzaSyCEL8kSwSyX4eIk6SeCQMtPGwCyzApvM2c'
+
+# authenticate flask
+app = Flask(__name__)
+socketio = SocketIO(app)
+# this is important to register users
+app.secret_key = 'ILIKEEGGS'
+
+# Using REST API by google to set up user Login and registration
 FIREBASE_SIGN_UP = f'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={Firebase_api_key}'
 FIREBASE_SIGN_in = f'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={Firebase_api_key}'
 
-# Flask app and SocketIO setup
-app = Flask(__name__)
-socketio = SocketIO(app)
-app.secret_key = 'ILIKEEGGS'
-
-# Firebase credentials and initialization
-cred = credentials.Certificate("Credentials.json")
-firebase_admin.initialize_app(cred, {"databaseURL": "https://alarm-notification-94a3c-default-rtdb.firebaseio.com/"})
-db_ref = db.reference()
-
-# Audio file path
-Audio_File_path = ""
-
-# Authentication and route protection
+# Setting up protection function for routes
+# Only registered users can have access to the database collection logs and Live feed
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -43,39 +40,52 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+# redirects you to the main feed on startup
 @app.route('/')
 @login_required
 def home():
     return render_template('index.html')
 
+# HTML page for the main video feed dashboard
 @app.route('/Video_page')
 @login_required
 def video_page():
     return render_template('Video.html')
 
+# redundant but just shows the camera feed without any dashboard/ui
 @app.route('/video_feed')
 @login_required
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+# Gets all the information from the fire base site in JSON format don't know if their another way will look into making it into a tabular form
 @app.route('/data')
 @login_required
 def data():
     try:
         data = db_ref.get()
-        return jsonify(data)
+        return render_template('data.html', data=data)
     except Exception as e:
         return jsonify({"error": str(e)})
 
+# For user creation on firebase
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        payload = {'email': email, 'password': password, 'returnSecureToken': True}
+
+        # create new user r
+        payload = {
+            'email': email,
+            'password': password,
+            'returnSecureToken': True
+        }
         try:
             response = requests.post(FIREBASE_SIGN_UP, json=payload)
             response_data = response.json()
+            print(response_data)
+
             if 'idToken' in response_data:
                 flash('user registered')
                 return redirect(url_for('login'))
@@ -86,15 +96,24 @@ def register():
             flash(f"error creating user :{str(e)}")
     return render_template('Register.html')
 
+# For user login using firebase api key
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        payload = {'email': email, 'password': password, 'returnSecureToken': True}
+
+        # Authenticate with Firebase
+        payload = {
+            'email': email,
+            'password': password,
+            'returnSecureToken': True
+        }
         try:
             response = requests.post(FIREBASE_SIGN_in, json=payload)
             response_data = response.json()
+            print(response_data)
+
             if 'idToken' in response_data:
                 session['id_token'] = response_data['idToken']
                 flash('login worked yh')
@@ -103,8 +122,10 @@ def login():
                 return render_template('login.html', error=response_data.get('error', {}).get('message', 'Unknown error'))
         except Exception as e:
             return render_template('login.html', error=str(e))
+
     return render_template('Login.html')
 
+# terminate current user session
 @app.route('/logout')
 @login_required
 def logout():
@@ -112,19 +133,51 @@ def logout():
     flash('you have logged out')
     return redirect(url_for('login'))
 
-def play_audio():
-    try:
-        playsound(Audio_File_path)
-    except Exception as e:
-        print(f"Error playing audio: {e}")
+# set firebase credentials
+# they are in the project directory
+cred = credentials.Certificate("Credentials.json")
 
+# Live firebase database link
+firebase_admin.initialize_app(cred, {"databaseURL": "https://alarm-notification-94a3c-default-rtdb.firebaseio.com/"})
+
+# Initialize data reference
+db_ref = db.reference()
+
+# pygame initialization alongside the audio play
+# defines the file path
+pygame.mixer.init()
+Audio_File_path = "Machine_gun.mp3"
+object_detected = False
+
+
+# creates a function to play audio in the loop
+# alongside error handling if the audio can not be found
+def play_audio():
+    global object_detected
+    pygame.mixer.set_num_channels(1)
+    # trigger conditions for the play audio function
+    if not object_detected:
+        try:
+            notification_sound = pygame.mixer.Sound(Audio_File_path)
+            notification_sound.play()
+        except Exception as e:
+            print(f"Error playing audio: {e}")
+
+# Load the labels from the config file in the directory
 with open('config.yaml', 'r') as file:
     class_labels = yaml.safe_load(file)
+
+# specify Labels and print them on the terminal for verification
 LABELS = [class_labels['names'][i] for i in range(len(class_labels['names']))]
 print(LABELS)
+
+# set list of colours to represent the class label
+np.random.seed(42)
 COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
 
+# file directory to the trained model
 model_path = os.path.join('.', 'runs', 'detect', 'train60', 'weights', 'best.pt')
+# Load a model
 try:
     model = YOLO(model_path)
     print("MODEL LOADED")
@@ -132,34 +185,47 @@ except Exception as e:
     print(f"Error loading model: {e}")
     exit()
 
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 30
-rawCapture = PiRGBArray(camera, size=(640, 480))
-import time
-time.sleep(0.1)
+# initialize the camera
+picam2 = Picamera2()
+camera_config = picam2.create_preview_configuration(main={"size": (640, 480)})
+picam2.configure(camera_config)
+picam2.start()
 
-PIR_SENSOR_PIN =17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(PIR_SENSOR_PIN,GPIO.IN)
-
+# Threshold score determines whether the desired object detected is similar to the one in the model
 threshold = 0.5
 
+# Main Loop function
 def generate_frames():
-if GPIO.input(PIR_SENSOR_PIN):
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        image = frame.array
-        h, w, _ = image.shape
-        results = model(image)[0]
+    while True:
+        frame = picam2.capture_array()
+        h, w, _ = frame.shape
+        results = model(frame)[0]
+        # setting the bounding box
         for result in results.boxes.data.tolist():
+
+            # attach the box alongside the accuracy and the id in this case phone
             x1, y1, x2, y2, score, class_id = result
+
+            # Adjust coordinates for relative position
+            x_min = int(max(0, x1))  # Ensure x1 doesn't go negative
+            y_min = int(max(0, y1))  # Ensure y1 doesn't go negative
+            x_max = int(min(h, x2))  # Ensure x2 doesn't exceed frame width
+            y_max = int(min(w, y2))  # Ensure y2 doesn't exceed frame height
+
+            # drawing of the bounding boxes and the class assignment
             if score > threshold:
-                socketio.emit('object_detected', {'message': f"{results.names[int(class_id)].upper()} detected!"})
-                cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
-                cv2.putText(image, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+                socketio.emit('object_detected', {'message': "WEAPON DETECTED!"})
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+                cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+
+                # calls the play audio function
                 play_audio()
+
+                # get current time stamp
                 timestamp = datetime.datetime.now().isoformat()
+
+                # creates the node structure that will be stored in the database
+                # format{key:value} it is possible to nest keys
                 device_info = {
                     "OS": platform.system(),
                     "Version": platform.version(),
@@ -172,19 +238,24 @@ if GPIO.input(PIR_SENSOR_PIN):
                     "Location": "Nigeria",
                     "Device": device_info
                 }
+
+                # write data to firebase Database
                 try:
+                    # Assuming db_ref.push returns a reference to the newly created node
                     db_ref.push(data)
                     print("Data pushed successfully!")
                 except Exception as e:
                     print(f"Error pushing data to database: {e}")
-        ret, buffer = cv2.imencode('.jpg', image)
-        frame_bytes = buffer.tobytes()
-        rawCapture.truncate(0)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' +
-               frame_bytes + b'\r\n')
 
+        ret, buffer = cv2.imencode('.jpg', frame)  # Changed to JPG for smaller size
+
+        frame_bytes = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+# run the flask instance
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', debug=True, allow_unsafe_werkzeug=True)
 
-camera.close()
+# free up memory
+picam2.stop()
